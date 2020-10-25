f3ad9ebeb19065bfa4c2fb877528eb7d013a10f8
Sam Gillingham <gillingham.sam@gmail.com>
2020-10-25 11:38:18 +1000

    add __future__ statement

--- rattree/rattree.py
+++ rattree/rattree.py
@@ -79,6 +79,8 @@ row 4 = [9, 0, 1]
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function, division
+
 from collections import OrderedDict
 import numpy
 import numba
